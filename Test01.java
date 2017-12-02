760422e1ee51edf21ad12b726b183e6ed947b97a
comeCU <1404842608@qq.com>
2017-12-02 13:56:06 +0800

    Test01forUpdata
    updata

---
+++ Test01.java
@@ -0,0 +1,15 @@
+package com.dong.test1201;
+
+public class Test01 {
+
+	/**测试使用GitHub，上传更改
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
